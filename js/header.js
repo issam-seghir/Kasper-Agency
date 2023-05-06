73d389abf18ab22b3b2c0e242e746cc9f587f47f
Issam-seghir <issamusma@hotmail.com>
2023-05-06 09:53:29 +0100

    big update

--- js/header.js
+++ js/header.js
@@ -1,27 +1,31 @@
-let navToggle = document.querySelector(".nav__toggle");
-let navWrapper = document.querySelector(".nav__wrapper");
+const navToggle = document.querySelector(".nav__toggle");
+const navWrapper = document.querySelector(".nav__wrapper");
 
 navToggle.addEventListener("click", function () {
-  if (navWrapper.classList.contains("active")) {
-    this.setAttribute("aria-expanded", "false");
-    this.setAttribute("aria-label", "menu");
-    navWrapper.classList.remove("active");
-  } else {
-    navWrapper.classList.add("active");
-    this.setAttribute("aria-label", "close menu");
-    this.setAttribute("aria-expanded", "true");
-    searchForm.classList.remove("active");
-  }
+    if (navWrapper.classList.contains("active")) {
+        this.setAttribute("aria-expanded", "false");
+        this.setAttribute("aria-label", "menu");
+        navWrapper.classList.remove("active");
+    } else {
+        navWrapper.classList.add("active");
+        this.setAttribute("aria-label", "close menu");
+        this.setAttribute("aria-expanded", "true");
+        searchForm.classList.remove("active");
+    }
 });
 
-let searchToggle = document.querySelector(".search__toggle");
-let searchForm = document.querySelector(".search__form");
+const searchToggle = document.querySelector(".search__toggle");
+const searchForm = document.querySelector(".search__form");
 
 searchToggle.addEventListener("click", showSearch);
 
 function showSearch() {
-  searchForm.classList.toggle("active");
-  navToggle.setAttribute("aria-expanded", "false");
-  navToggle.setAttribute("aria-label", "menu");
-  navWrapper.classList.remove("active");
+    searchForm.classList.toggle("active");
+    navToggle.setAttribute("aria-expanded", "false");
+    navToggle.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
 }
+
+/* eslint quotes: ["error", "double"] */
+const a = "b";
+const b = "c";
